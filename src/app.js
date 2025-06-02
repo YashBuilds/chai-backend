5a2952b6e3fd5ab91b1d57394206517c216bc34a
UBER-VIDEO <yashvloggs20@gmail.com>
2025-06-03 01:38:13 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -21,4 +21,15 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// routes import 
+
+import userRouter from './routes/user.routes.js'
+
+
+
+//routes declaration -- router ko app alg rkhe ho ab router ko lane ke liye middleware lana padega
+app.use("/api/v1/users",userRouter)   //jse hi userRouter bolega toh control de dege userRouter pe.. ar bolega wha pe ky kam karna hai kis route pe user ko leke jana hai
+
+//    http://localhost:8000/api/v1/users/register
+
 export {  app }
